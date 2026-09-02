87cb23ea85db3ebcbafcefb8bf63cd0262c2fff5
Wojko <21193625+wojko6@users.noreply.github.com>
2026-09-02 10:09:16 +0200

    Satisfy ShellCheck for procfs UID parsing

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -5,7 +5,7 @@ set -u
 PATH="/opt/sbin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 current_uid() {
-    while read -r status_key status_uid status_rest; do
+    while read -r status_key status_uid _; do
         if [ "$status_key" = "Uid:" ]; then
             printf '%s\n' "$status_uid"
             return 0
