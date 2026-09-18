d2ac17584aae881297a00b20794bb84866414191
Wojko <21193625+wojko6@users.noreply.github.com>
2026-09-18 19:50:16 +0200

    test: use explicit Fedora DR command mocks

--- tests/test-fedora-dr-restore.sh
+++ tests/test-fedora-dr-restore.sh
@@ -27,7 +27,7 @@ EOF
 chmod +x "$FAKEBIN/findmnt" "$FAKEBIN/blkid"
 
 output="$(
-    PATH="$FAKEBIN:/usr/sbin:/usr/bin:/sbin:/bin"     FEDORA_DR_PATH="$FAKEBIN:/usr/sbin:/usr/bin:/sbin:/bin"     "$SCRIPT" "$TARGET" --dry-run
+    FEDORA_DR_FINDMNT="$FAKEBIN/findmnt"     FEDORA_DR_BLKID="$FAKEBIN/blkid"     "$SCRIPT" "$TARGET" --dry-run
 )"
 
 printf '%s\n' "$output" | grep -F 'Detected /boot UUID: NEW-BOOT-UUID' >/dev/null
